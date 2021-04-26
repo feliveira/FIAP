8b288dca1479e83deab30286e00fe6cf3b8dc9a8
Felipe Oliveira <61992864+feliveira@users.noreply.github.com>
2021-04-26 19:10:38 -0300

    Felicidade

--- ifood.py
+++ ifood.py
@@ -1,10 +1,9 @@
 # Restaurantes participantes
-restaurantes = [["Sukiya - Saúde", 4.6, 2.5],
+restaurantes = [["Sukiya - Saúde", 4.6, 7.99],
                 ["A Feijoada", 4.4, 9.90],
-                ["Makis Place - Saúde", 4.7, 7,99],
                 ["Kibon Sorveteria - Saúde", 4.9, 6.99],
-                ["Lanchonete do Clebinho",4.9, 2.5],
                 ["Giraffas Carrefour Metrocar", 4.4, 5.99],
+                ["Makis Place - Saúde", 4.7, 7.99],
                 ["Viena - Shopping Santa Cruz", 4.4, 12.49]]
 
 # Cria lista para armazenar as notas do restaurante
@@ -28,33 +27,24 @@ while len(lista_notas) > 0:
             restaurantes_por_notas.append(maior)
             lista_notas.pop(0)
 
-
-
 # ============================ LIDAR COM O FRETE ==========================
 
-# Cria lista para armazenar os valores dos fretes
-lista_fretes = []
+# Percorre a lista ordenada por notas começando do segundo item
+for x in range(1,len(restaurantes_por_notas)):  
+# Compara a nota do segundo item da lista com a do primeiro, se ambas as notas forem iguais,
+# lidará com o critério de desempate: o frete
+ if restaurantes_por_notas[x][1] == restaurantes_por_notas[x-1][1]:
+   # Verifica se o valor do frete do segundo termo é menor que a do anterior
+   if restaurantes_por_notas[x][2] < restaurantes_por_notas[x-1][2]:
+     # Se a condição for verdadeira utiliza a variável "restaurante" para realizar as trocas de posições
+     # Salva o restaurante de frete menor
+     restaurante = restaurantes_por_notas[x-1]
+     # Coloca o restaurante de maior frete na posição seguinte
+     restaurantes_por_notas[x-1] = restaurantes_por_notas[x]
+     # Substitui a posição anterior com o restaurante de menor frete 
+     restaurantes_por_notas[x] = restaurante
 
-# Percorre a lista de restaurantes ordenados por nota e armazena em outra lista o tempo do frete
-for frete in restaurantes_por_notas:
-    lista_fretes.append(frete[2])
-
-# Ordena a lista de tempo do frete em ordem crescente
-lista_fretes.sort()
-
-print(lista_fretes)
-
-while len(lista_fretes) > 0:
-    for frete in restaurantes_por_notas:
-        if frete[2] == lista_fretes[0]:
-            restaurantes_ordenados.append(frete)
-            lista_fretes.pop(0)
-
-print(restaurantes_ordenados)
-
-
-
-
-
-
+for x in range(len(restaurantes_por_notas)):
+    print("{}° {} | Nota: {} | Valor do Frete: R$ {}".format(x+1,restaurantes_por_notas[x][0],restaurantes_por_notas[x][1],restaurantes_por_notas[x][2]))
+    
 
